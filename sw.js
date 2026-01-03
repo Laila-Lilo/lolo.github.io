354e1eb492752ee13c6e5b8e9ccfe3eecf87e064
Laila-Lilo <lailaafouad2017@gmail.com>
2026-01-03 20:36:36 +0200

    Implement service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "lolo games-v2";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
